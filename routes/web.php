6a22fd39e83c3512ed78d3114a14af2b2b843280
Arinaldi Surya <arinaldisurya374@gmail.com>
2025-04-30 15:54:53 +0800

    Mencoba memanggil tahun akademik di halaman web

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/', [HomeController::class, 'login'])->name('login');
 Route::get('/akademik', [HomeController::class, 'akademik'])->name('akademik')->middleware('auth');
-Route::get('/tahunakademik', [HomeController::class, 'tahunakademik'])->name('tahunakademik');
+Route::get('/tahunakademik', [HomeController::class, 'indexThnAk'])->name('tahunakademik');
 Route::get('/kelas', [HomeController::class, 'kelas'])->name('kelas');
 Route::get('/mahasiswa', [HomeController::class, 'mahasiswa'])->name('mahasiswa');
 
